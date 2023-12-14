f55990aae6adfe16ca74b8fd9a665fb5c3b465d9
Ahmad Hanafi <83533499+ahmad123365@users.noreply.github.com>
2023-12-14 20:56:39 +0200

    added back error handling functions

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-// process.on('uncaughtException', err => {
-//   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-//   console.log(err);
-//   process.exit(1);
-// });
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err);
+  process.exit(1);
+});
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
@@ -21,10 +21,10 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-// process.on('unhandledRejection', err => {
-//   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-//   console.log(err);
-//   server.close(() => {
-//     process.exit(1);
-//   });
-// });
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
